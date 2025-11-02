c119b56b7cfeacec4efeaaaa8e77f629559ea1ae
durubeyhan-gif <duru.beyhan@std.yeditepe.edu.tr>
2025-11-02 13:51:30 +0300

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -2,17 +2,10 @@ import numpy as np
 
 def find_period(L0, L1):
     g = 9.81
-    
     for L in range(L0, L1 + 1):
         T = 2 * np.pi * np.sqrt(L / g)
         print(f"When L = {L:4.1f} m, T = {T:.1f} s")
-    
     T0 = 2 * np.pi * np.sqrt(L0 / g)
     T1 = 2 * np.pi * np.sqrt(L1 / g)
-    
     return T0, T1
-    
-    print("\nResults:")
-    print(f"T0 (L0={L0} m) = {T0:.2f} s")
-    print(f"T1 (L1={L1} m) = {T1:.2f} s")
-    
+find_period(2, 10)
